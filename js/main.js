d741edd9f2f7826464e74eb0a3fefbdbbbe1ddd4
artfoundry <dave@artfoundry.com>
2014-06-13 17:51:54 -0700

    mapdata is now stored as a 2D array of objects instead of numbers

--- js/main.js
+++ js/main.js
@@ -1,7 +1,4 @@
 //submodules
-//(function(){
-//    var map = angular.module("map", []);
-//})
 //
 //(function(){
 //    var tiles = angular.module("tiles", []);
@@ -20,26 +17,29 @@
 //})
 //
 
+var mapSizeX = 20;
+var mapSizeY = 10;
+
 angular.module("rogueCraft.movePlayer", []).value("player", {
     move: function($event) {
         var $currentTile = $(".tile-current");
-        var tileDown = parseInt($currentTile.text(), 10) + 10;
-        var tileUp = parseInt($currentTile.text(), 10) - 10;
+        var tileDown = parseInt($currentTile.text(), 10) + mapSizeX;
+        var tileUp = parseInt($currentTile.text(), 10) - mapSizeX;
         var tileLeft = parseInt($currentTile.text(), 10) - 1;
         var tileRight = parseInt($currentTile.text(), 10) + 1;
         var newTile = parseInt($($event.target).text());
         switch (newTile) {
             case tileDown:
-                if (tileDown <= 100) { this.changeTile($event); }
+                if (tileDown <= (mapSizeX * mapSizeY)) { this.changeTile($event); }
                 break;
             case tileUp:
                 if (tileUp >= 0) { this.changeTile($event); }
                 break;
             case tileLeft:
-                if (parseInt($currentTile.text()) % 10 !== 1) { this.changeTile($event); }
+                if (parseInt($currentTile.text()) % mapSizeX !== 1) { this.changeTile($event); }
                 break;
             case tileRight:
-                if (parseInt($currentTile.text()) % 10 !== 0) { this.changeTile($event); }
+                if (parseInt($currentTile.text()) % mapSizeX !== 0) { this.changeTile($event); }
                 break;
             default: return;
         }
@@ -49,34 +49,44 @@ angular.module("rogueCraft.movePlayer", []).value("player", {
         $($event.target).attr("class", "tile tile-current");
     },
     start: function() {
-        var startTile = Math.round(Math.random() * 100);
+        var startTile = Math.round(Math.random() * (mapSizeX * mapSizeY));
         $(function () {
             $(".tile:contains(' " + startTile + " ')").addClass("tile-current");
         });
+        return startTile;
     }
 });
 
-angular.module("rogueCraft.createLevel", []).value("level", {
-    tileNum: [],
-    create: function() {
-        for (var i = 0; i < 10; i++) {
-            this.tileNum[i] = [];
+angular.module("rogueCraft.mapData", []).value("map", {
+    mapData: [],
+    createGrid: function() {
+        for (var i = 0; i < mapSizeY; i++) {
+            this.mapData[i] = [];
             var lastNum = 0;
-            for (var j = 0; j < 10; j++) {
-                i === 0 ? lastNum = 0 : lastNum = this.tileNum[i-1][9];
-                this.tileNum[i][j] = j + lastNum + 1;
+            for (var j = 0; j < mapSizeX; j++) {
+                i === 0 ? lastNum = 0 : lastNum = this.mapData[i-1][mapSizeX-1]["tileNum"];
+                this.mapData[i][j] = { tileNum: j + lastNum + 1 };
             }
+        }
+    },
+    createMap: function(startTile) {
+        var tiles = mapSizeX * mapSizeY;
+        var count = 1;
+        while (count <= tiles) {
+
+            count += 1;
         }
     }
 });
 
 //app module
-var rogueCraft = angular.module("rogue-craft", ["rogueCraft.createLevel", "rogueCraft.movePlayer"]).run(function(level, player){
-    level.create();
-    player.start();
+var rogueCraft = angular.module("rogue-craft", ["rogueCraft.mapData", "rogueCraft.movePlayer"]).run(function(map, player){
+    map.createGrid();
+    var startTile = player.start();
+    map.createMap(startTile);
 });
 
-rogueCraft.controller("RogueController", function($scope, player, level){
-    $scope.tileNum = level.tileNum;
+rogueCraft.controller("RogueController", function($scope, player, map){
+    $scope.mapData = map.mapData;
     $scope.handleClick = function($event) { player.move($event) };
 });
