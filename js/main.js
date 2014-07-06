4c46b687554b71d2ada0d1d4c239dc871681c30b
artfoundry <dave@artfoundry.com>
2014-07-06 15:22:46 -0700

    random functionality code is done but not tested; need to figure out passing data between modules with services

--- js/main.js
+++ js/main.js
@@ -17,38 +17,36 @@
 //})
 //
 
-var mapSizeCols = 20;
-var mapSizeRows = 10;
-var numOfTiles = mapSizeCols * mapSizeRows;
-var edgeTypes = ["wall", "door", "blank"];
+var mapSizeRows = 10;   // number of rows in the map
+var mapSizeCols = 20;   // number of columns in the map
+var numOfTiles = mapSizeRows * mapSizeCols;
 
 // PLAYER module
 
 angular.module("rogueCraft.movePlayer", []).value("player", {
-    move: function($event, $scope.mapData) {
-//        var $currentTile = $(".tile-current");
-        var tileDown = $scope.mapData["tileNum"] + mapSizeCols;
-        var tileUp = $scope.mapData["tileNum"] - mapSizeCols;
+    move: function($event, $scope) {
+        var tileDown = $scope.mapData["tileNum"] + mapSizeRows;
+        var tileUp = $scope.mapData["tileNum"] - mapSizeRows;
         var tileLeft = $scope.mapData["tileNum"] - 1;
         var tileRight = $scope.mapData["tileNum"] + 1;
         var newTile = parseInt($($event.target).text());
         switch (newTile) {
             case tileDown:
-                if (tileDown <= (mapSizeCols * mapSizeRows)) { this.changeTile($event); }
+                if (tileDown <= (mapSizeRows * mapSizeCols)) { this.changeTile($event); }
                 break;
             case tileUp:
                 if (tileUp >= 0) { this.changeTile($event); }
                 break;
             case tileLeft:
-                if ($scope.mapData["tileNum"] % mapSizeCols !== 1) { this.changeTile($event); }
+                if ($scope.mapData["tileNum"] % mapSizeRows !== 1) { this.changeTile($event); }
                 break;
             case tileRight:
-                if ($scope.mapData["tileNum"] % mapSizeCols !== 0) { this.changeTile($event); }
+                if ($scope.mapData["tileNum"] % mapSizeRows !== 0) { this.changeTile($event); }
                 break;
             default: return;
         }
     },
-    changeTile: function($event){
+    changeTile: function($event) {
         $(".tile-current").removeClass("tile-current").addClass("tile-visited");
         $($event.target).attr("class", "tile tile-current");
     },
@@ -58,9 +56,8 @@ angular.module("rogueCraft.movePlayer", []).value("player", {
 
 angular.module("rogueCraft.mapData", []).value("map", {
     totalDungeonLevels: 20,
-    dungeonData: new Array(totalDungeonLevels),
+    dungeonData: new Array(this.totalDungeonLevels),
     mapData: [],
-//    pathData: ["wall", "wall", "wall", "wall"],
     createGrid: function() {
         for (var i = 0; i < mapSizeRows; i++) {
             this.mapData[i] = [];
@@ -71,77 +68,58 @@ angular.module("rogueCraft.mapData", []).value("map", {
             }
         }
     },
-    createMap: function(startTile) {
+    createMap: function(startTileLoc) {
         var count = 1;
         var adjTileData = {};
         var adjTileEdge = 0;
-        var currentTile = startTile;    // array of two numbers indicating col and row for tile location - not sure it's needed - only being used to pass to getNextUnfinishedTile
-        var currentTileData = this.mapData[startTile[0]][startTile[1]]; // tile object (hash) containing edge info, etc.
-        var edge;
+        var currentTileLoc = startTileLoc;    // array of two numbers indicating col and row for tile location - not sure it's needed - only being used to pass to getNextUnfinishedTile
+        var currentTileData = this.mapData[startTileLoc[0]][startTileLoc[1]]; // tile object (hash) containing edge info, etc.
+        currentTileData["edges"] = []; // indices go clockwise from top just like css box model; options are "wall", "door", "blank"
         var currentTileEdges = currentTileData["edges"];
+        var edge = 0;
+        var nextTileObject = {};
         this.mapData["path"] = [];  // dynamic array of objects which are pushed to array starting with start tile, each object containing array(4) and pointer to tile hash in mapData
         this.mapData["path"].push(currentTileData);
 
         while (count <= numOfTiles) {
-            currentTileData["edges"] = new Array(4); // indices go clockwise from top just like css box model; options are "wall", "door", "blank"
-            var nextTileObject = {};
-            for (edge = 0; edge < 4; edge++) {
+            currentTileEdges = new Array(4); // indices go clockwise from top just like css box model; options are "wall", "door", "blank"
+            nextTileObject = {};
+            edge = 0;
+            while (nextTileObject === {} && edge < 4) {
                 // if tile is in top row and edge is at top OR if tile is in bottom row and edge is at bottom OR if tile is on left column and edge is on left OR if tile is on right column and edge is on right
-                if (((currentTileData["tileNum"] < mapSizeCols) && (edge === 0)) || ((currentTileData["tileNum"] > numOfTiles - mapSizeCols) && (edge === 2)) || ((currentTileData["tileNum"] % mapSizeCols === 1) && (edge === 3)) || ((currentTileData["tileNum"] % mapSizeCols === 0) && (edge === 1))) {
-                    currentTileEdges[edge] = {edgeType: edgeTypes[0]}; // then set edge to be a wall
+                if (((currentTileData["tileNum"] < mapSizeRows) && (edge === 0)) || ((currentTileData["tileNum"] > numOfTiles - mapSizeRows) && (edge === 2)) || ((currentTileData["tileNum"] % mapSizeRows === 1) && (edge === 3)) || ((currentTileData["tileNum"] % mapSizeRows === 0) && (edge === 1))) {
+                    currentTileEdges[edge] = {edgeType: "wall"}; // then set edge to be a wall
                 } else if (currentTileEdges[edge] === undefined) {
-                    adjTileData = this.getNextTileData(currentTile, edge);
+                    adjTileData = this.getNextTileData(currentTileLoc, edge);
                     adjTileEdge = this.getNextTileEdge(adjTileData, edge);
                     currentTileEdges[edge] = {edgeType: adjTileEdge, adjTile: adjTileData};
-                    if (adjTileEdge !== "wall") {
-                        this.addTileToTree(adjTileData, currentTileEdges, edge);
-                    };
-                    if (nextTileObject === {}) {
-                        nextTileObject = this.setPathNextTile(adjTileData);
-                    };
-                };
+                    if (adjTileEdge !== "wall") { this.addTileToTree(adjTileData, currentTileEdges, edge); }
+                    if (nextTileObject === {}) { nextTileObject = this.getPreviousTileOnPath(); }
+                }
             // need to deal with what happens when we reach a dead end
+                edge += 1;
             }
+            if (nextTileObject === {}) { nextTileObject = this.addTileToPath(adjTileData); }
             currentTileData = nextTileObject["tileData"];
-            currentTile = nextTileObject["tileLoc"];    // unnecessary?
+            currentTileLoc = nextTileObject["tileLoc"];
             count += 1;
         }
         this.dungeonData.push(this.mapData);
     },
-    // this method needs to track every tile visited so it can return to previous tile once hitting a dead end
-//    getNextUnfinishedTile: function(currentTileData, currentTile) {
-//        var edge = 0;
-//        var wallCount = 0;
-//        mapData["tilesArray"].push(currentTile);
-//    // need to be able to know which edge is matching with the previous adjacent tile so we don't set that tile as the next one (exception is the start tile the first time)
-//
-//        while (mapData["tilesArray"].length < numOfTiles) {
-//            while (currentTileEdges[edge] === "wall") {
-//                edge += 1;
-//            }
-//            if (edge < 4) { // if there's a non-wall edge
-//                var nextTileObject = {tileLoc: mapData["tilesArray"].slice(-1), tileData: this.getNextTileData(edge)}
-//                return nextTileObject;
-//            } else {    //otherwise we've reached a dead end so backtrack
-//                this.getPreviousTile();
-//            };
-//
-//        }
-//    },
-    getNextTileData: function(currentTile, edge) {
+    getNextTileData: function(currentTileLoc, edge) {
         var adjTileData = {};
         switch(edge) {
             case 0:
-                adjTileData = this.mapData[currentTile[0]][currentTile[1] - 1];
+                adjTileData = this.mapData[currentTileLoc[0]][currentTileLoc[1] - 1];
                 break;
             case 1:
-                adjTileData = this.mapData[currentTile[0] + 1][currentTile[1]];
+                adjTileData = this.mapData[currentTileLoc[0] + 1][currentTileLoc[1]];
                 break;
             case 2:
-                adjTileData = this.mapData[currentTile[0]][currentTile[1] + 1];
+                adjTileData = this.mapData[currentTileLoc[0]][currentTileLoc[1] + 1];
                 break;
             case 3:
-                adjTileData = this.mapData[currentTile[0] - 1][currentTile[1]];
+                adjTileData = this.mapData[currentTileLoc[0] - 1][currentTileLoc[1]];
                 break;
             default:
                 break;
@@ -166,13 +144,13 @@ angular.module("rogueCraft.mapData", []).value("map", {
             default:
                 break;
         }
-        if (adjTileEdge === undefined) {
-            adjTileEdge = edgeTypes[Math.floor(Math.random() * 3];
-        }
+        var edgeTypes = ["wall", "door", "blank"];
+        if (adjTileEdge === undefined) { adjTileEdge = edgeTypes[Math.floor(Math.random() * 3)]; }
         return adjTileEdge;
     },
-    getPreviousTile: function() {
-
+    getPreviousTileOnPath: function() {
+        this.mapData["path"].pop();
+        return this.mapData["path"][-1];
     },
     addTileToTree: function(newTileData, currentTileEdges, edge) {
         var oppEdge;
@@ -180,29 +158,27 @@ angular.module("rogueCraft.mapData", []).value("map", {
         edge < 2 ? oppEdge = edge + 2 : oppEdge = edge - 2;
         newTileData["edges"][oppEdge] = currentTileEdges;
     },
-    setPathNextTile: function(newTileData, edge) {
+    addTileToPath: function(newTileData, edge) {
         this.mapData["path"].push(newTileData);
         return this.mapData["path"][-1];
     },
-    setPathPreviousTile: function(tileData, edge) {
-
-    },
     start: function() {
-        var startTile = new Array(2);
-        startTile[0] = Math.floor(Math.random() * (mapSizeCols * mapSizeRows + 1)); // row
-        startTile[1] = Math.floor(Math.random() * (mapSizeCols * mapSizeRows + 1)); // column
-        $(function () {
-            $(".tile:contains(' " + mapData[startTileX][startTileY]["tileNum"] + " ')").addClass("tile-current");
-        });
-        return startTile;
+        var startTileLoc = new Array(2);
+        startTileLoc[0] = Math.floor(Math.random() * (mapSizeRows + 1)); // X
+        startTileLoc[1] = Math.floor(Math.random() * (mapSizeCols + 1)); // Y
+
+        console.log(this.mapData, startTileLoc)
+        var that = this;
+        $(function() { $(".tile:contains(' " + that.mapData[startTileLoc[0]][startTileLoc[1]]["tileNum"] + " ')").addClass("tile-current"); });
+        return startTileLoc;
     }
 });
 
 //app module
 var rogueCraft = angular.module("rogue-craft", ["rogueCraft.mapData", "rogueCraft.movePlayer"]).run(function(map, player){
     map.createGrid();
-    var startTile = map.start();
-    map.createMap(startTile);
+    var startTileLoc = map.start();
+    map.createMap(startTileLoc);
 });
 
 rogueCraft.controller("RogueController", function($scope, player, map){
