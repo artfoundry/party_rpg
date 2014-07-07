0d523af05ff2b4f01bd9c854230f27d82cbc0eed
artfoundry <dave@artfoundry.com>
2014-07-07 00:48:02 -0700

    adds a wandering monster, ability to click on it to kill it, score tracking, and end game if monster lands on player

--- js/main.js
+++ js/main.js
@@ -20,30 +20,76 @@
 //})
 //
 
+angular.module("rogueCraft.moveMonster", []).value("monster", {
+    move: function() {
+        var $monsterTile = $(".tile-monster");
+        var nextTile = [];
+        var choice = Math.floor(Math.random() * 4);
+        var monsterTileVal = parseInt($monsterTile.text(), 10);
+        nextTile[0] = parseInt($monsterTile.text(), 10) + 10;
+        nextTile[1] = parseInt($monsterTile.text(), 10) - 10;
+        nextTile[2] = parseInt($monsterTile.text(), 10) - 1;
+        nextTile[3] = parseInt($monsterTile.text(), 10) + 1;
+        if (monsterTileVal <= 10) { nextTile[1] = nextTile[0]; }
+        else if (monsterTileVal >= 91) { nextTile[0] = nextTile[1]; }
+        else if (monsterTileVal % 10 === 0) { nextTile[3] = nextTile[2]; }
+        else if (monsterTileVal % 10 === 1) { nextTile[2] = nextTile[3]; }
+        this.changeTile(nextTile[choice]);
+    },
+    changeTile: function(newTile){
+        $(".tile-monster").removeClass("tile-monster tile-sword tile-splat");
+        $(".tile").filter(function() {
+            return parseInt($(this).text(), 10) === newTile;
+        }).addClass("tile-monster");
+        if ($(".tile-monster").hasClass("tile-current")) {
+            alert("The monster ate you! Click OK to reload and play again.");
+            location.reload();
+        }
+        if ($(".tile-monster").hasClass("tile-option")) {
+            $(".tile-monster").removeClass("tile-option").addClass("tile-sword");
+        }
+    },
+    start: function() {
+        var startTile = Math.floor(Math.random() * 100 + 1);
+        $(function () {
+            $(".tile:contains(' " + startTile + " ')").addClass("tile-monster");
+        });
+    }
+});
+
 angular.module("rogueCraft.movePlayer", []).value("player", {
+    score: 0,
     move: function($event) {
         var $currentTile = $(".tile-current");
         var tileDown = parseInt($currentTile.text(), 10) + 10;
         var tileUp = parseInt($currentTile.text(), 10) - 10;
         var tileLeft = parseInt($currentTile.text(), 10) - 1;
         var tileRight = parseInt($currentTile.text(), 10) + 1;
-        var newTile = parseInt($($event.target).text());
-        switch (newTile) {
-            case tileDown:
-                if (tileDown <= 100) { this.changeTile($event); }
-                break;
-            case tileUp:
-                if (tileUp >= 0) { this.changeTile($event); }
-                break;
-            case tileLeft:
-                if (parseInt($currentTile.text()) % 10 !== 1) { this.changeTile($event); }
-                break;
-            case tileRight:
-                if (parseInt($currentTile.text()) % 10 !== 0) { this.changeTile($event); }
-                break;
-            default: return;
+        var clickedTile = parseInt($($event.target).text(), 10);
+        if ($($event.target).hasClass("tile-monster") && ((clickedTile === tileDown) || (clickedTile === tileUp) || (clickedTile === tileLeft) || (clickedTile === tileRight))) {
+            this.score += 1;
+            $($event.target).removeClass("tile-monster tile-sword");
+            $($event.target).addClass("tile-splat");
+            return "win";
+        } else {
+            var newTile = parseInt($($event.target).text());
+            switch (newTile) {
+                case tileDown:
+                    if (tileDown <= 100) { this.changeTile($event); }
+                    break;
+                case tileUp:
+                    if (tileUp >= 0) { this.changeTile($event); }
+                    break;
+                case tileLeft:
+                    if (parseInt($currentTile.text()) % 10 !== 1) { this.changeTile($event); }
+                    break;
+                case tileRight:
+                    if (parseInt($currentTile.text()) % 10 !== 0) { this.changeTile($event); }
+                    break;
+                default: return;
+            }
+            this.markTile();
         }
-        this.markTile();
     },
     changeTile: function($event){
         var $currentTile = $(".tile-current");
@@ -70,11 +116,19 @@ angular.module("rogueCraft.movePlayer", []).value("player", {
         var tileUp = parseInt($currentTile.text(), 10) - 10;
         var tileLeft = parseInt($currentTile.text(), 10) - 1;
         var tileRight = parseInt($currentTile.text(), 10) + 1;
-        $(".tile:contains('" + tileDown + "')").addClass("tile-option");
-        $(".tile:contains('" + tileUp + "')").addClass("tile-option");
-        $(".tile:contains('" + tileLeft + "')").addClass("tile-option");
-        $(".tile:contains('" + tileRight + "')").addClass("tile-option");
-    }
+        $(".tile").filter(function() {
+            return parseInt($(this).text(), 10) === tileDown;
+        }).addClass("tile-option");
+        $(".tile").filter(function() {
+            return parseInt($(this).text(), 10) === tileUp;
+        }).addClass("tile-option");
+        $(".tile").filter(function() {
+            return parseInt($(this).text(), 10) === tileLeft;
+        }).addClass("tile-option");
+        $(".tile").filter(function() {
+            return parseInt($(this).text(), 10) === tileRight;
+        }).addClass("tile-option");
+    },
 });
 
 angular.module("rogueCraft.createLevel", []).value("level", {
@@ -92,13 +146,17 @@ angular.module("rogueCraft.createLevel", []).value("level", {
 });
 
 //app module
-var rogueCraft = angular.module("rogue-craft", ["rogueCraft.createLevel", "rogueCraft.movePlayer"]).run(function(level, player){
+var rogueCraft = angular.module("rogue-craft", ["rogueCraft.createLevel", "rogueCraft.movePlayer", "rogueCraft.moveMonster"]).run(function(level, player, monster){
     level.create();
     player.start();
     $(function() { player.markTile(); });
+    monster.start();
 });
 
-rogueCraft.controller("RogueController", function($scope, player, level){
+rogueCraft.controller("RogueController", function($scope, player, monster, level){
     $scope.tileNum = level.tileNum;
-    $scope.handleClick = function($event) { player.move($event) };
+    $scope.player = player;
+    $scope.handleClick = function($event) {
+        if (player.move($event) === "win") { monster.start(); }
+        monster.move(); };
 });
